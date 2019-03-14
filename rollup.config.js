99c083d2b267055d8d97d9d4d22f5d67f9f7d35a
damingerdai <mingguobin@live.com>
2019-03-14 21:53:15 +0800

    feat: 使用babel编译

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
-import typescript from 'rollup-plugin-typescript'
+import typescript from 'rollup-plugin-typescript';
+import babel from 'rollup-plugin-babel';
 
 export default {
     input: 'src/main.ts',
     output: {
         file: 'lib/ip.js',
-        format: 'cjs'
+        format: 'cjs',
     },
     plugins: [
-        typescript({ lib: ['es5', 'es6', 'dom'], target: 'es5' })
+        typescript({ lib: ['es5', 'es6', 'dom'], target: 'es5' }),
+        babel()
     ]
 };
