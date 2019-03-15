8ff6513c91fb350e2291e91100889ac33295669e
damingeridai <mingguobin@live.com>
2019-03-15 21:32:23 +0800

    build: 支持基于es6模块的构建

--- rollup.es.config.js
+++ rollup.es.config.js
@@ -4,8 +4,9 @@ import babel from 'rollup-plugin-babel';
 export default {
     input: 'src/main.ts',
     output: {
-        file: 'lib/bundles/ip.js',
-        format: 'cjs',
+        file: 'lib/es5/ip.js',
+        format: 'es',
+        sourcemap: true
     },
     plugins: [
         typescript({ lib: ['es5', 'es6', 'dom'], target: 'es5', declaration: true }),
