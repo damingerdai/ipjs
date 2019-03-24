70cbea98b914d5dbb5a271dbdd1d2f51f730e510
明国宾 <mingguobin@live.com>
2019-03-24 23:46:06 +0800

    build: 添加获取ipv6的地址方法但是没有测试条件

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,20 @@ export function getIp() {
     });
     return IPv4 || '127.0.0.1';
 }
+
+export function getIpv4() {
+    return getIp();
+}
+
+export function getIpv6() {
+    let IPv6;
+    const data = os.networkInterfaces();
+    Object.keys(data).forEach(key => {
+        data[key].forEach(ip => {
+            if (ip.family === 'IPv6') {
+                IPv6 = ip.address;
+            }
+        });
+    });
+    return IPv6;
+}
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,6 +1,23 @@
-import { getIp } from '../src/main';
+import { getIp, getIpv4, getIpv6 } from '../src/main';
+
+const ipv4 = /^((\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.){4}$/;
+const ipv6 = /^(([\da-fA-F]{1,4}):){8}$/;
 
 test('getIp', () => {
     const ip = getIp();
     expect(ip).toMatch(/^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$/);
+});
+
+test('getIpv4', () => {
+    const ip = getIpv4();
+    expect(ip).toMatch(/^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$/);
+});
+
+test('getIpv6', () => {
+    const ip = getIpv6();
+    if (ip) {
+        console.warn('no ipv6');
+    } else {
+        expect(ip).toMatch(ipv6);
+    }
 });
